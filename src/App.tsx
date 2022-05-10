d71203e7bd29bc4518c32a1979c600551ab86ef8
Tom Pretty <tompretty2@gmail.com>
2022-05-10 16:00:48 +0100

    add basic interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,247 @@
-import React from "react";
-
-type Letter =
-  | "A"
-  | "B"
-  | "C"
-  | "D"
-  | "E"
-  | "F"
-  | "G"
-  | "H"
-  | "I"
-  | "J"
-  | "K"
-  | "L"
-  | "M"
-  | "N"
-  | "O"
-  | "P"
-  | "Q"
-  | "R"
-  | "S"
-  | "T"
-  | "U"
-  | "V"
-  | "W"
-  | "X"
-  | "Y"
-  | "Z";
-
-interface WordWheelDefinition {
-  centerLetter: Letter;
-  outerLetters: [
-    Letter,
-    Letter,
-    Letter,
-    Letter,
-    Letter,
-    Letter,
-    Letter,
-    Letter
-  ];
-}
-
-interface WordWheelState {
-  words: String[];
-}
-
-export interface WordWheel {
-  definition: WordWheelDefinition;
-  state: WordWheelState;
-}
+import React, { useState } from "react";
+import { WordWheel, WordWheelDefinition, WordWheelLogic } from "./logic";
+import "./App.css";
+import { WORDS } from "./words";
 
 const WORD_WHEEL_DEFINITION: WordWheelDefinition = {
   centerLetter: "A",
   outerLetters: ["C", "T", "G", "T", "E", "A", "I", "S"],
 };
 
+const logic = new WordWheelLogic(WORDS);
+
+type GuessElement = "CENTER" | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+type Guess = GuessElement[];
+
 function App() {
+  const [wordWheel, setWordWheel] = useState<WordWheel>({
+    definition: WORD_WHEEL_DEFINITION,
+    state: { words: [] },
+  });
+
+  const [guess, setGuess] = useState<Guess>([]);
+
+  function toggleGuessElement(element: GuessElement) {
+    if (guess.includes(element)) {
+      setGuess(guess.filter((el) => el !== element));
+    } else {
+      setGuess([...guess, element]);
+    }
+  }
+
+  function submit() {
+    const update = logic.update(
+      wordWheel,
+      guessToWord(guess, wordWheel.definition)
+    );
+
+    if (update.valid) {
+      setWordWheel(update.wordWheel);
+      setGuess([]);
+    }
+  }
+
+  function reset() {
+    setGuess([]);
+  }
+
+  function deleteLastElement() {
+    setGuess(guess.slice(0, -1));
+  }
+
   return (
     <div>
       <header>Word Wheel</header>
+      <p>Words: {wordWheel.state.words.join(", ")}</p>
+      <p>Word: {guessToWord(guess, WORD_WHEEL_DEFINITION)}</p>
+
+      <div>
+        <button onClick={submit}>Submit</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={deleteLastElement}>Delete</button>
+      </div>
+
+      <WordWheelWheel
+        definition={WORD_WHEEL_DEFINITION}
+        guess={guess}
+        toggleGuessElement={toggleGuessElement}
+      />
     </div>
   );
+}
+
+// ---- Helper components ---- //
+
+interface WordWhelWheelProps {
+  definition: WordWheelDefinition;
+  guess: Guess;
+  toggleGuessElement: (element: GuessElement) => void;
+}
+
+function WordWheelWheel({
+  definition,
+  guess,
+  toggleGuessElement,
+}: WordWhelWheelProps) {
+  function isSelected(element: GuessElement) {
+    return guess.includes(element);
+  }
+
+  return (
+    <svg viewBox="-50 -50 100 100" width="500">
+      <g>
+        <circle
+          cx="0"
+          cy="0"
+          r="48"
+          stroke="black"
+          strokeWidth="2px"
+          fill="none"
+        />
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(0)}>
+          <path
+            d={`M 0 0 L -48 0 A 48 48 0 0 1 -33.9411255 -33.9411255 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(0)}
+          />
+
+          <text x="-32.3357836" y="-13.3939201" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[0]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(1)}>
+          <path
+            d={`M 0 0 L -33.9411255 -33.9411255 A 48 48 0 0 1 0 -48 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(1)}
+          />
+
+          <text x="-13.3939201" y="-32.3357836" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[1]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(2)}>
+          <path
+            d={`M 0 0 L 0 -48 A 48 48 0 0 1 33.9411255 -33.9411255 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(2)}
+          />
+
+          <text x="13.3939201" y="-32.3357836" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[2]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(3)}>
+          <path
+            d={`M 0 0 L 33.9411255 -33.9411255 A 48 48 0 0 1 48 0 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(3)}
+          />
+
+          <text x="32.3357836" y="-13.3939201" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[3]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(4)}>
+          <path
+            d={`M 0 0 L 48 0 A 48 48 0 0 1 33.9411255 33.9411255 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(4)}
+          />
+
+          <text x="32.3357836" y="13.3939201" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[4]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(5)}>
+          <path
+            d={`M 0 0 L 33.9411255 33.9411255 A 48 48 0 0 1 0 48 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(5)}
+          />
+
+          <text x="13.3939201" y="32.3357836" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[5]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(6)}>
+          <path
+            d={`M 0 0 L 0 48 A 48 48 0 0 1 -33.9411255 33.9411255 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(6)}
+          />
+
+          <text x="-13.3939201" y="32.3357836" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[6]}
+          </text>
+        </g>
+
+        <g className="wheel-segment" onClick={() => toggleGuessElement(7)}>
+          <path
+            d={`M 0 0 L -33.9411255 33.9411255 A 48 48 0 0 1 -48 0 Z`}
+            stroke="black"
+            strokeWidth="2px"
+            fill="None"
+            data-selected={isSelected(7)}
+          />
+
+          <text x="-32.3357836" y="13.3939201" textAnchor="middle" dy=".4em">
+            {definition.outerLetters[7]}
+          </text>
+        </g>
+      </g>
+
+      <g className="wheel-segment" onClick={() => toggleGuessElement("CENTER")}>
+        <circle
+          cx="0"
+          cy="0"
+          r="20"
+          stroke="black"
+          strokeWidth="2px"
+          fill="white"
+          data-selected={isSelected("CENTER")}
+        />
+
+        <text x="0" y="0" textAnchor="middle" dy=".3em">
+          {definition.centerLetter}
+        </text>
+      </g>
+    </svg>
+  );
+}
+
+// ---- Helper functions ---- //
+
+function guessToWord(guess: Guess, definition: WordWheelDefinition) {
+  const letters = guess.map((el) => {
+    if (el === "CENTER") {
+      return definition.centerLetter;
+    }
+    return definition.outerLetters[el];
+  });
+
+  return letters.join("");
 }
 
 export default App;
